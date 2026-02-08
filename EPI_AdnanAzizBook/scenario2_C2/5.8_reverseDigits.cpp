33af9b3360aab066182195cbcd7e44289debe4cf
Alam, Faraz <farazalam030@live.in>
2026-02-08 19:10:46 +0000

    Add EPI_book 5.8 Reverse Digits Problem

---
+++ EPI_AdnanAzizBook/scenario2_C2/5.8_reverseDigits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+long Reverse(int x)
+{
+    bool is_neg = x<0;
+    long result = 0, x_remaining=abs(x);
+    while(x_remaining)
+    {
+        result = result * 10 + x_remaining % 10;
+        x_remaining /=10 ;     
+    }
+    return is_neg ?  -result : result; 
+}
+
+int main (){
+
+    int n = -123;
+    std::cout << "reverseOf("<<n<<") = " <<Reverse(n)<<std::endl; 
+
+    return 0;
+}
